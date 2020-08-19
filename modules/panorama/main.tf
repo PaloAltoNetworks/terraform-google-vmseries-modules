9c18ef9f1d1307f88841d8dc2e9c9fe4697c2029
Konrad Bechler <kbechler@paloaltonetworks.com>
2020-08-19 14:45:28 +0200

    initial commit

---
+++ modules/panorama/main.tf
@@ -0,0 +1,78 @@
+# --- create bucket and upload panorama source image file ---
+resource "google_storage_bucket" "panorama-bucket" {
+  name                     = var.panorama_bucket_name
+  default_event_based_hold = false
+  location                 = var.region
+}
+
+resource "google_storage_bucket_object" "panorama-bucket-image" {
+  name       = var.panorama_image_file_name
+  source     = "${var.panorama_image_file_path}/${var.panorama_image_file_name}"
+  bucket     = var.panorama_bucket_name
+  depends_on = [ google_storage_bucket.panorama-bucket ]
+}
+
+# --- create panorama VM image ---
+resource "google_compute_image" "panorama-image" {
+  name = var.image
+  raw_disk {
+    container_type = "TAR"
+    source         = "${var.storage_uri}/${var.panorama_bucket_name}/${var.panorama_image_file_name}?authuser=0"
+  }
+  timeouts {
+    create = var.image_create_timeout
+  }
+  depends_on = [ google_storage_bucket_object.panorama-bucket-image ]
+
+}
+
+# --- create network interfaces for panorama ---
+resource "google_compute_address" "nic0" {
+  count  = length(var.names)
+  name   = "${element(var.names, count.index)}-nic0"
+  region = var.region
+}
+
+# --- create panorama instances ---
+resource "google_compute_instance" "panorama" {
+  count                     = length(var.names)
+  name                      = element(var.names, count.index)
+  machine_type              = var.machine_type
+  zone                      = element(var.zones, count.index)
+  min_cpu_platform          = var.cpu_platform
+  can_ip_forward            = true
+  allow_stopping_for_update = true
+  tags                      = var.tags
+
+  metadata = {
+    # panorama-bootstrap-gce-storagebucket = var.bootstrap_bucket
+    serial-port-enable = true
+    ssh-keys           = var.ssh_key
+  }
+
+  service_account {
+    scopes = var.scopes
+  }
+
+  network_interface {
+    dynamic "access_config" {
+      for_each = var.nic0_public_ip ? [""] : []
+      content {
+        nat_ip = google_compute_address.nic0[count.index].address
+      }
+    }
+    network_ip = element(var.nic0_ip, count.index)
+    subnetwork = var.subnetworks[0]
+  }
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+      type  = var.disk_type
+    }
+  }
+  
+  depends_on = [
+      google_compute_image.panorama-image
+  ]
+}
---
+++ modules/panorama/outputs.tf
@@ -0,0 +1,11 @@
+# output "vm_names" {
+#   value = google_compute_instance.panorama.*.name
+# }
+
+# output "vm_self_link" {
+#   value = google_compute_instance.panorama.*.self_link
+# }
+
+# output "nic0_public_ip" {
+  # value = var.nic0_public_ip ? google_compute_instance.panorama.*.network_interface.0.access_config.0.nat_ip : []
+# }
