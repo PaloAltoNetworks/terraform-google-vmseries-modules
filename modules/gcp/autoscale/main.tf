576a7b7b2f74d59e22549234253659b076776521
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2020-08-21 10:21:59 +0200

    fix minor

--- modules/gcp/autoscale/main.tf
+++ modules/gcp/autoscale/main.tf
@@ -74,67 +74,6 @@ resource "google_compute_instance_template" "this" {
   }
 }
 
-//resource "google_compute_region_instance_group_manager" "this" {
-//  # the '-igm-' is allegedly a magic string for Panorama
-//  name                      = "${var.prefix}-igm-${var.region}a"
-//  base_instance_name        = "${var.prefix}-fw"
-//  region                    = var.region
-//  distribution_policy_zones = var.zones
-//
-//  version {
-//    instance_template = google_compute_instance_template.this.id
-//  }
-//
-//  target_pools = [var.pool]
-//
-//  named_port {
-//    name = "custom"
-//    port = 80
-//  }
-//
-//
-//
-//  # auto_healing_policies {
-//  #   health_check      = google_compute_health_check.autohealing.id
-//  #   initial_delay_sec = 300
-//  # }
-//}
-
-
-
-//resource "random_id" "autoscaler" {
-//  keepers = {
-//    # Re-randomize on igm change. It forcibly recreates all users of this random_id.
-//    google_compute_region_instance_group_manager = google_compute_region_instance_group_manager.this.id
-//  }
-//  byte_length = 3
-//}
-
-
-//resource "google_compute_region_autoscaler" "this" {
-//  name   = "${var.prefix}-${random_id.autoscaler.hex}-autoscaler"
-//  region = var.region
-//  target = google_compute_region_instance_group_manager.this.id
-//
-//  autoscaling_policy {
-//    max_replicas = 2
-//    min_replicas = 2
-//    # FIXME
-//    # Given that it takes 7 minutes for a PA-VM to become functional, we need a cool down time
-//    # period of 10 minutes (600 seconds) for a new autoscale event to kick in.
-//    cooldown_period = 30
-//
-//    # cpu_utilization { target = 0.7 }
-//
-//    metric {
-//      name   = var.autoscaler_metric_name
-//      type   = var.autoscaler_metric_type
-//      target = var.autoscaler_metric_target
-//    }
-//
-//  }
-//}
-
 resource "google_compute_instance_group_manager" "this" {
   for_each           = var.zoning
   base_instance_name = "${var.prefix}-fw"
