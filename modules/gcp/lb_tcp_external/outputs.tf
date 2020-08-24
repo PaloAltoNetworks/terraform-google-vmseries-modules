62d9a67705467110325dbd045ee89001908c2988
Konrad Bechler <kbechler@paloaltonetworks.com>
2020-08-24 13:09:30 +0200

    use lb_tcp_external instead of direct resources in main.tf

--- modules/gcp/lb_tcp_external/outputs.tf
+++ modules/gcp/lb_tcp_external/outputs.tf
@@ -5,3 +5,7 @@ output forwarding_rule {
 output forwarding_rule_ip_address {
   value = google_compute_forwarding_rule.default.ip_address
 }
+
+output target_pool {
+  value = google_compute_target_pool.default.self_link
+}
