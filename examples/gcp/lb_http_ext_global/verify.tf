2dc0c2a1071d1370ea2749aa54cfe100120f7ab4
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2020-08-31 12:35:47 +0200

    add example verify.tf

---
+++ examples/gcp/lb_http_ext_global/verify.tf
@@ -0,0 +1,25 @@
+resource "null_resource" "verify_with_curl" {
+  for_each = module.vm.nic0_public_ip
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      host        = each.value
+      private_key = file("~/.ssh/id_rsa")
+      user        = "demo"
+    }
+
+    inline = [
+      "curl -sSi http://${module.glb.address} | head -1",
+      "curl -sSi http://${module.extlb.forwarding_rule_ip_address} | head -1",
+    ]
+  }
+
+  triggers = {
+    run_me_every_time = "${timestamp()}"
+  }
+}
+
+output "regional_url" {
+  value = "http://${module.extlb.forwarding_rule_ip_address}"
+}
