f463b12b75f057221f298e8426c20022d71dd9df
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2020-08-31 12:34:53 +0200

    add gcp/vm output nic0

--- modules/gcp/vm/outputs.tf
+++ modules/gcp/vm/outputs.tf
@@ -9,3 +9,7 @@ output vm_self_link {
 output instance_group {
   value = google_compute_instance_group.default.*.self_link
 }
+
+output nic0_public_ip {
+  value = { for k, v in google_compute_instance.default : k => v.network_interface.0.access_config.0.nat_ip }
+}
