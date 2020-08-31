31c4486c1f6ef84e45ff31080c1552ab9f0782f3
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2020-08-31 12:32:00 +0200

    refactor example move vm

--- examples/gcp/lb_http_ext_global/main.tf
+++ examples/gcp/lb_http_ext_global/main.tf
@@ -1,3 +1,19 @@
+module "vm" {
+  source      = "../../../modules/gcp/vm/"
+  names       = ["my-vm01", "my-vm02"]
+  zones       = [data.google_compute_zones.available.names[0], data.google_compute_zones.available.names[1]]
+  subnetworks = ["my-subnet", "my-subnet"]
+
+  ## Any image will do, if only it exposes on port 80 the http url `/`:
+  image        = "https://console.cloud.google.com/compute/imagesDetail/projects/nginx-public/global/images/nginx-plus-centos7-developer-v2019070118"
+  machine_type = "g1-small"
+
+  ## The part before the colon is the ssh user name. The part after is intended to be replaced with your own ssh-rsa public key.
+  ssh_key = "demo:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCbUVRz+1iNWsTVly/Xou2BUe8+ZEYmWymClLmFbQXsoFLcAGlK+NuixTq6joS+svuKokrb2Cmje6OyGG2wNgb8AsEvzExd+zbNz7Dsz+beSbYaqVjz22853+uY59CSrgdQU4a5py+tDghZPe1EpoYGfhXiD9Y+zxOIhkk+RWl2UKSW7fUe23UdXC4f+YbA0+Xy2l19g/tOVFgThHJn9FFdlQqlJC6a/0mWfudRNLCaiO5IbOlXIKvkLluWZ2GIMkr8uC5wldHyutF20EdAF9A4n72FssHCvB+WhrMCLspIgMfQA3ZMEfQ+/N5sh0c8vCZXV8GumlV4rN9xhjLXtTwf"
+
+  create_instance_group = true
+}
+
 #########################################################################
 # Global HTTP Load Balancer
 
