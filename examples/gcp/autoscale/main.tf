c0d9119ccf6b78cee022704e375e88a1957b5b96
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2020-09-14 11:29:09 +0200

    add examples/gcp/autoscale

---
+++ examples/gcp/autoscale/main.tf
@@ -0,0 +1,123 @@
+terraform {
+  required_version = ">= 0.12, < 0.13"
+}
+
+provider "google" {
+  version = "= 3.35"
+  region  = var.region
+}
+
+data "google_project" "this" {}
+data "google_compute_zones" "this" {}
+
+#-----------------------------------------------------------------------------------------------
+# Create  buckets for bootstrapping the fresh firewall VM.
+
+module "bootstrap" {
+  source          = "../../../modules/gcp/gcp_bootstrap/"
+  bucket_name     = "as4-fw-bootstrap"
+  service_account = var.service_account
+  file_location   = "bootstrap_files/"
+  config          = ["init-cfg.txt"]
+  license         = ["authcodes"]
+}
+
+#-----------------------------------------------------------------------------------------------
+# Firewalls with auto-scaling.
+
+module "autoscale" {
+  source          = "../../../modules/gcp/autoscale"
+  region          = var.region
+  prefix          = var.prefix
+  deployment_name = var.prefix
+  project_id      = data.google_project.this.name
+  zoning = {
+    zone1 = data.google_compute_zones.this.names[0]
+    zone2 = data.google_compute_zones.this.names[1]
+  }
+  subnetworks = [
+    var.untrust_subnet[0],
+    var.mgmt_subnet[0],
+    var.trust_subnet[0],
+  ]
+  machine_type             = var.fw_machine_type
+  mgmt_interface_swap      = "enable"
+  ssh_key                  = fileexists(var.public_key_path) ? "admin:${file(var.public_key_path)}" : ""
+  image                    = "${var.fw_image}-${var.fw_panos}"
+  nic0_public_ip           = true
+  nic1_public_ip           = true
+  nic2_public_ip           = false
+  pool                     = google_compute_target_pool.this.self_link
+  bootstrap_bucket         = module.bootstrap.bucket_name
+  scopes                   = ["https://www.googleapis.com/auth/cloud-platform"]
+  service_account          = var.service_account
+  max_replicas_per_zone    = 2
+  autoscaler_metric_name   = var.autoscaler_metric_name
+  autoscaler_metric_type   = var.autoscaler_metric_type
+  autoscaler_metric_target = var.autoscaler_metric_target
+
+  dependencies = [
+    module.bootstrap.completion,
+  ]
+}
+
+#-----------------------------------------------------------------------------------------------
+# The Internal LB is not strictly required part of this example.
+# It just shows how to integrate with auto-scaling.
+
+module "intlb" {
+  source            = "../../../modules/gcp/lb_tcp_internal/"
+  name              = var.intlb_name
+  network           = var.trust_vpc
+  subnetworks       = var.trust_subnet
+  all_ports         = true
+  ports             = []
+  health_check_port = "22"
+
+  backends = {
+    "0" = [
+      {
+        group    = module.autoscale.instance_group_manager["zone1"].instance_group
+        failover = false
+      },
+      {
+        group    = module.autoscale.instance_group_manager["zone2"].instance_group
+        failover = false
+      }
+    ]
+  }
+}
+
+#-----------------------------------------------------------------------------------------------
+# The External TCP Load Balancer
+# It is not strictly required part of this example.
+# It just shows how to integrate with auto-scaling.
+
+resource "google_compute_forwarding_rule" "this" {
+  name                  = var.extlb_name
+  target                = google_compute_target_pool.this.self_link
+  load_balancing_scheme = "EXTERNAL"
+  region                = var.region
+  ip_protocol           = "TCP"
+  port_range            = "80"
+}
+
+resource "google_compute_target_pool" "this" {
+  name             = var.extlb_name
+  region           = var.region
+  session_affinity = "NONE" // Options are `NONE`, `CLIENT_IP` and `CLIENT_IP_PROTO`
+  health_checks    = [google_compute_http_health_check.this.self_link]
+}
+
+resource "google_compute_http_health_check" "this" {
+  name = "${var.prefix}-hc"
+
+  check_interval_sec  = 10
+  timeout_sec         = 5
+  unhealthy_threshold = 3
+  healthy_threshold   = 2
+
+  port = var.extlb_healthcheck_port
+  # request_path        = var.health_check["request_path"]
+  # host                = var.health_check["host"]
+}
---
+++ examples/gcp/autoscale/variables.tf
@@ -0,0 +1,111 @@
+# variable "project_id" {
+#   description = "GCP Project ID"
+#   type        = string
+# }
+
+# variable "auth_file" {
+#   description = "GCP Project auth JSON file"
+#   type        = string
+# }
+
+variable region {
+  description = "GCP Region"
+  default     = "europe-west4"
+  type        = string
+}
+
+variable public_key_path {
+  description = "Local path to public SSH key. To generate the key pair use `ssh-keygen -t rsa -C admin -N '' -f id_rsa`  If you do not have a public key, run `ssh-keygen -f ~/.ssh/demo-key -t rsa -C admin`"
+  default     = "id_rsa.pub"
+}
+
+variable fw_panos {
+  description = "VM-Series license and PAN-OS (ie: bundle1-814, bundle2-814, or byol-814)"
+  default     = "byol-913"
+}
+
+variable fw_image {
+  default = "https://www.googleapis.com/compute/v1/projects/paloaltonetworksgcp-public/global/images/vmseries"
+}
+
+variable fw_machine_type {
+  default = "n1-standard-4"
+}
+
+variable prefix {
+  description = "Prefix to GCP resource names, an arbitrary string"
+  default     = "as4"
+  type        = string
+}
+
+variable extlb_name {
+  default = "as4-fw-extlb"
+}
+
+variable extlb_healthcheck_port {
+  type    = number
+  default = 80
+}
+
+variable intlb_name {
+  default = "as4-fw-intlb"
+}
+
+variable mgmt_sources {
+  default = ["0.0.0.0/0"]
+  type    = list(string)
+}
+
+variable mgmt_vpc {
+}
+
+variable mgmt_subnet {
+  type = list(string)
+}
+
+# variable mgmt_cidr {
+#   type = list(string)
+# }
+
+variable untrust_vpc {
+}
+
+variable untrust_subnet {
+  type = list(string)
+}
+
+# variable untrust_cidr {
+#   type = list(string)
+# }
+
+variable trust_vpc {
+  description = "Pre-existing VPC that contains the services and apps"
+  type        = string
+}
+
+variable trust_subnet {
+  description = "Pre-existing subnets that contain the services and apps"
+  type        = list(string)
+}
+
+# variable trust_cidr {
+#   type = list(string)
+# }
+
+variable autoscaler_metric_name {
+  default = "custom.googleapis.com/VMSeries/panSessionActive"
+}
+
+variable autoscaler_metric_type {
+  default = "GAUGE"
+}
+
+variable autoscaler_metric_target {
+  default = 100
+}
+
+variable service_account {
+  description = "IAM Service Account for running firewall instance (just the email)"
+  default     = null
+  type        = string
+}
