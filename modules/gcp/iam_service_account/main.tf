91754e752d99679ca60e9ca8bb58efaa1c146c92
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2020-09-15 15:25:13 +0200

    refactor gcp/iam_service_account for_each

--- modules/gcp/iam_service_account/main.tf
+++ modules/gcp/iam_service_account/main.tf
@@ -3,28 +3,8 @@ resource "google_service_account" "this" {
   display_name = var.display_name
 }
 
-resource "google_project_iam_member" "logging_logWriter" {
-  role   = "roles/logging.logWriter"
-  member = "serviceAccount:${google_service_account.this.email}"
-}
-
-resource "google_project_iam_member" "monitoring_metricWriter" {
-  role   = "roles/monitoring.metricWriter"
-  member = "serviceAccount:${google_service_account.this.email}"
-}
-
-resource "google_project_iam_member" "monitoring_viewer" {
-  role   = "roles/monitoring.viewer"
-  member = "serviceAccount:${google_service_account.this.email}"
-}
-
-resource "google_project_iam_member" "stackdriver_resourceMetadata_writer" {
-  role   = "roles/stackdriver.resourceMetadata.writer"
-  member = "serviceAccount:${google_service_account.this.email}"
-}
-
-# per https://docs.paloaltonetworks.com/vm-series/9-1/vm-series-deployment/set-up-the-vm-series-firewall-on-google-cloud-platform/deploy-vm-series-on-gcp/enable-google-stackdriver-monitoring-on-the-vm-series-firewall.html
-resource "google_project_iam_member" "stackdriver_accounts_viewer" {
-  role   = "roles/stackdriver.accounts.viewer"
-  member = "serviceAccount:${google_service_account.this.email}"
+resource "google_project_iam_member" "this" {
+  for_each = var.roles
+  role     = each.value
+  member   = "serviceAccount:${google_service_account.this.email}"
 }
