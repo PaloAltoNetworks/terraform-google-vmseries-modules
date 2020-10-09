64195df156b15ae110abe54f408d9ab1fbc29b1f
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2020-10-09 16:08:28 +0200

    refactor vpc, data depends_on
    Konrad's way of always going through the `data` objects, because
    code becomes much more readable.

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -3,36 +3,27 @@ locals {
   networks = { for v in var.networks : v.name => v } // tested on tf-0.12, when list elements shift indexes, this map prevents destroy
 
   // Some networks need to be created:
-  networks_to_create = { for k, v in local.networks
+  networks_to_create = {
+    for k, v in local.networks
     : k => v
     if ! (try(v.create_network == false, false))
-  }
-
-  // But other networks already exist:
-  networks_to_gather = { for k, v in local.networks
-    : k => v
-    if try(v.create_network == false, false)
   }
 
   // Same code, but now for subnetworks:
   subnetworks = { for v in var.networks : "${v.name}-${var.region}" => v }
 
   // Some subnetworks need to be created:
-  subnetworks_to_create = { for k, v in local.subnetworks
+  subnetworks_to_create = {
+    for k, v in local.subnetworks
     : k => v
     if ! (try(v.create_subnetwork == false, false))
-  }
-
-  // But other subnetworks already exist:
-  subnetworks_to_gather = { for k, v in local.subnetworks
-    : k => v
-    if try(v.create_subnetwork == false, false)
   }
 }
 
 data "google_compute_network" "this" {
-  for_each = local.networks_to_gather
-  name     = each.value.name
+  for_each   = local.networks
+  name       = each.value.name
+  depends_on = [google_compute_network.this]
 }
 
 resource "google_compute_network" "this" {
@@ -43,23 +34,24 @@ resource "google_compute_network" "this" {
 }
 
 data "google_compute_subnetwork" "this" {
-  for_each = local.subnetworks_to_gather
-  name     = try(each.value.subnetwork_name, "${each.value.name}-${var.region}")
-  region   = var.region
+  for_each   = local.subnetworks
+  name       = try(each.value.subnetwork_name, "${each.value.name}-${var.region}")
+  region     = var.region
+  depends_on = [google_compute_subnetwork.this]
 }
 
 resource "google_compute_subnetwork" "this" {
   for_each      = local.subnetworks_to_create
   name          = try(each.value.subnetwork_name, "${each.value.name}-${var.region}")
   ip_cidr_range = each.value.ip_cidr_range
-  network       = try(google_compute_network.this[each.value.name].self_link, data.google_compute_network.this[each.value.name].self_link)
+  network       = data.google_compute_network.this[each.value.name].self_link
   region        = var.region
 }
 
 resource "google_compute_firewall" "this" {
   for_each      = { for k, v in local.networks : k => v if can(v.allowed_sources) }
   name          = "${each.value.name}-ingress"
-  network       = try(google_compute_network.this[each.key].self_link, data.google_compute_network.this[each.key].self_link)
+  network       = data.google_compute_network.this[each.key].self_link
   direction     = "INGRESS"
   source_ranges = each.value.allowed_sources
 
