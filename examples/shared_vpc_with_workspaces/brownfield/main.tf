6d2aa54d975b90b0866df68130cda05fc771019e
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2020-10-15 16:32:29 +0200

    fix brownfield comments

--- examples/shared_vpc_with_workspaces/brownfield/main.tf
+++ examples/shared_vpc_with_workspaces/brownfield/main.tf
@@ -6,14 +6,24 @@ provider "google" {
   version = "= 3.35"
 }
 
-#-----------------------------------------------------------------------------------------------
-# The brownfield is the code that is normally not here.
+# The brownfield is the pre-existing infrastructure.
+# Although we create it as Terraform objects, the rest of Terraform code doesn't use them.
+# The same objects can also be created in Google Cloud Console or by API.
+
+# The VPC Networks.
 module "brownfield" {
   source   = "../../../modules/vpc"
   networks = var.brownfield_networks
   region   = var.brownfield_networks_region
 }
 
-# FIXME: terraform destroy --target module.vpc.data.google_compute_network.this
-# "0 destroyed"
-# Now apply can gather the data.
+# The pre-existing public IP address. 
+# We need to migrate it onto PANW VM-Series.
+resource "google_compute_address" "this" {
+  name   = "my-example4-ip"
+  region = var.brownfield_networks_region
+}
+
+output "public_ip_allocatted" {
+  value = google_compute_address.this.address
+}
