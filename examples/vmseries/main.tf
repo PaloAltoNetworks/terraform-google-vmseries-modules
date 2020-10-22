a426cdf5af8d332b2fa3f76f565d0e6fcc42a356
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2020-10-22 21:03:36 +0200

    feat(examples): rm nicspec

--- examples/vmseries/main.tf
+++ examples/vmseries/main.tf
@@ -44,22 +44,28 @@ module "vpc" {
   ]
 }
 
-locals {
-  nic_attributes_list = [
-    { public_nat = true },
-    { public_nat = true },
-    { public_nat = false, ip_address = "192.168.2.15" },
-  ]
-}
-
 # Spawn the VM-series firewall as a Google Cloud Engine Instance.
 module "vmseries" {
   source = "../../modules/vmseries"
   instances = {
     "my-example-fw01" = {
-      name               = "my-example3-fw01"
-      zone               = data.google_compute_zones.this.names[2]
-      network_interfaces = [for k, v in module.vpc.nicspec : merge(v, local.nic_attributes_list[k])]
+      name = "my-example3-fw01"
+      zone = data.google_compute_zones.this.names[2]
+      network_interfaces = [
+        {
+          subnetwork = module.vpc.subnetworks["my-example3-untrust"].self_link
+          public_nat = true
+        },
+        {
+          subnetwork = module.vpc.subnetworks["my-example3-mgmt"].self_link
+          public_nat = true
+        },
+        {
+          subnetwork = module.vpc.subnetworks["my-example3-trust"].self_link
+          public_nat = false
+          ip_address = "192.168.2.15"
+        },
+      ]
     }
   }
   ssh_key = "admin:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCbUVRz+1iNWsTVly/Xou2BUe8+ZEYmWymClLmFbQXsoFLcAGlK+NuixTq6joS+svuKokrb2Cmje6OyGG2wNgb8AsEvzExd+zbNz7Dsz+beSbYaqVjz22853+uY59CSrgdQU4a5py+tDghZPe1EpoYGfhXiD9Y+zxOIhkk+RWl2UKSW7fUe23UdXC4f+YbA0+Xy2l19g/tOVFgThHJn9FFdlQqlJC6a/0mWfudRNLCaiO5IbOlXIKvkLluWZ2GIMkr8uC5wldHyutF20EdAF9A4n72FssHCvB+WhrMCLspIgMfQA3ZMEfQ+/N5sh0c8vCZXV8GumlV4rN9xhjLXtTwf"
