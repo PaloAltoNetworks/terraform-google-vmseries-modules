4fbc69e79ac25125a0fb3478ec4e544b0dfb9b98
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2020-10-26 12:26:22 +0100

    fix: deprecated syntax
    Replace this.0.x with this[0].x to satisfy a tflint rule. Reference:
    https://github.com/terraform-linters/tflint/blob/v0.20.2/docs/rules/terraform_deprecated_index.md
    
    Use the `this` naming convention.
    
    Use the neat coalesce().
    
    Format the code.

--- modules/lb_tcp_external/outputs.tf
+++ modules/lb_tcp_external/outputs.tf
@@ -1,11 +1,14 @@
 output forwarding_rule {
-  value = google_compute_forwarding_rule.default.*.self_link
+  description = "The self-link of the forwarding rule."
+  value       = google_compute_forwarding_rule.this.self_link
 }
 
 output address {
-  value = google_compute_forwarding_rule.default.ip_address
+  description = "The IP address of the forwarding rule."
+  value       = google_compute_forwarding_rule.this.ip_address
 }
 
 output target_pool {
-  value = google_compute_target_pool.default.self_link
+  description = "The self-link of the target pool."
+  value       = google_compute_target_pool.this.self_link
 }
