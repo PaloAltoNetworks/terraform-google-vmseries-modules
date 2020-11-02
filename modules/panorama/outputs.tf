ce9401ab14b2f759a19a15bc22915113de7c921a
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2020-11-02 13:58:53 +0100

    feat(panorama): output

--- modules/panorama/outputs.tf
+++ modules/panorama/outputs.tf
@@ -1,11 +1,3 @@
-# output "vm_names" {
-#   value = google_compute_instance.panorama.*.name
-# }
-
-# output "vm_self_link" {
-#   value = google_compute_instance.panorama.*.self_link
-# }
-
-# output "nic0_public_ip" {
-  # value = var.nic0_public_ip ? google_compute_instance.panorama.*.network_interface.0.access_config.0.nat_ip : []
-# }
+output "nic0_public_ips" {
+  value = var.nic0_public_ip ? google_compute_instance.this[*].network_interface[0].access_config[0].nat_ip : google_compute_instance.this[*].network_interface[0].ip_address
+}
