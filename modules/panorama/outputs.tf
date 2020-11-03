58096e8d52aa9218f08a7ea5db536451b7752d08
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2020-11-03 09:47:46 +0100

    feat(panorama): many subnets, for_each

--- modules/panorama/outputs.tf
+++ modules/panorama/outputs.tf
@@ -1,3 +1,11 @@
 output "nic0_public_ips" {
-  value = var.nic0_public_ip ? google_compute_instance.this[*].network_interface[0].access_config[0].nat_ip : google_compute_instance.this[*].network_interface[0].ip_address
+  value = var.nic0_public_ip ? { for k, v in google_compute_instance.this : k =>
+    v.network_interface[0].access_config[0].nat_ip
+  } : {}
+}
+
+output "nic0_private_ips" {
+  value = { for k, v in google_compute_instance.this : k =>
+    v.network_interface[0].network_ip
+  }
 }
