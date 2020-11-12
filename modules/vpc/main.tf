f65dfc5df1f664cb1b6c2256004ae4d550b7be5f
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2020-11-12 12:08:42 +0100

    fix(vpc): remove depends_on, output with try()
    Make the code focused on Terraform 0.12.x.
    It means `data { depends_on }` must be removed: for 0.12.29, it had
    quite dire consequences as tested:
    
    - Never run `terraform refresh`, it forces a subsequent
    destroy/recreate of dependent resources. Recover from it by restoring
    the tfstate from backup. No workaround available, which means that the
    production environment can never be refreshed to fully re-read the
    currently existing cloud resources.
    - The `apply` will also destroy/recreate of multiple resources.
    - For applying, always use: `terraform apply --refresh=false`
    - For planning, always use: `terraform plan --refresh=false`
    
    For outputs I use `try()` so that they are usable in subsequent
    for_each. Internally, `merge()` seems to cause no problems, because
    the code does not call `for_each`. Details:
    https://github.com/PaloAltoNetworks/terraform-best-practices/blob/d7865cd85fcf58b83c86cd816a12441e8aaec003/empty_state_and_tf12.md
    
    While `try()` hides errors behind it, which is not great, the `merge()`
    will hopefully reveal some of them.

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,6 +2,13 @@ locals {
   // All the networks:
   networks = { for v in var.networks : v.name => v } // tested on tf-0.12, when list elements shift indexes, this map prevents destroy
 
+  // Some networks already exist:
+  networks_existing = {
+    for k, v in local.networks
+    : k => v
+    if try(v.create_network == false, false)
+  }
+
   // Some networks need to be created:
   networks_to_create = {
     for k, v in local.networks
@@ -9,8 +16,15 @@ locals {
     if ! (try(v.create_network == false, false))
   }
 
-  // Same code, but now for subnetworks:
+  // We have networks, now the same for subnetworks:
   subnetworks = { for v in var.networks : v.subnetwork_name => v }
+
+  // Some subnetworks already exist:
+  subnetworks_existing = {
+    for k, v in local.subnetworks
+    : k => v
+    if try(v.create_subnetwork == false, false)
+  }
 
   // Some subnetworks need to be created:
   subnetworks_to_create = {
@@ -21,10 +35,9 @@ locals {
 }
 
 data "google_compute_network" "this" {
-  for_each   = local.networks
-  name       = each.value.name
-  project    = try(each.value.host_project_id, null)
-  depends_on = [google_compute_network.this]
+  for_each = local.networks_existing
+  name     = each.value.name
+  project  = try(each.value.host_project_id, null)
 }
 
 resource "google_compute_network" "this" {
@@ -36,25 +49,24 @@ resource "google_compute_network" "this" {
 }
 
 data "google_compute_subnetwork" "this" {
-  for_each   = local.subnetworks
-  name       = each.value.subnetwork_name
-  project    = try(each.value.host_project_id, null)
-  region     = var.region
-  depends_on = [google_compute_subnetwork.this]
+  for_each = local.subnetworks_existing
+  name     = each.value.subnetwork_name
+  project  = try(each.value.host_project_id, null)
+  region   = var.region
 }
 
 resource "google_compute_subnetwork" "this" {
   for_each      = local.subnetworks_to_create
   name          = each.value.subnetwork_name
   ip_cidr_range = each.value.ip_cidr_range
-  network       = data.google_compute_network.this[each.value.name].self_link
+  network       = merge(google_compute_network.this, data.google_compute_network.this)[each.value.name].self_link
   region        = var.region
 }
 
 resource "google_compute_firewall" "this" {
   for_each      = { for k, v in local.networks : k => v if can(v.allowed_sources) }
   name          = "${each.value.name}-ingress"
-  network       = data.google_compute_network.this[each.key].self_link
+  network       = merge(google_compute_network.this, data.google_compute_network.this)[each.key].self_link
   direction     = "INGRESS"
   source_ranges = each.value.allowed_sources
 
