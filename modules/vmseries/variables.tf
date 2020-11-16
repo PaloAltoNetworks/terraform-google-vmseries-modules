f538bc05bba4b673eca43ee3c50b04f34dea5b83
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2020-11-16 16:32:15 +0100

    refactor(vmseries): fmt

--- modules/vmseries/variables.tf
+++ modules/vmseries/variables.tf
@@ -8,8 +8,8 @@ variable instances {
 
 variable machine_type {
   description = "Firewall instance machine type, which depends on the license used. See the [Terraform manual](https://www.terraform.io/docs/providers/google/r/compute_instance.html)"
-  default = "n1-standard-4"
-  type    = string
+  default     = "n1-standard-4"
+  type        = string
 }
 
 variable min_cpu_platform {
