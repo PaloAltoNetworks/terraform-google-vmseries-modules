170e2960da87751e84b442efe52ef96b53b5c63a
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2020-11-18 16:23:34 +0100

    feat(vmseries): outputs nic0_ips etc

--- modules/vmseries/outputs.tf
+++ modules/vmseries/outputs.tf
@@ -6,28 +6,68 @@ output self_links {
   value = { for k, v in google_compute_instance.this : k => v.self_link }
 }
 
+output instances {
+  value = google_compute_instance.this
+}
+
+output instance_groups {
+  value = google_compute_instance_group.this
+}
+
+output instance_group_self_links {
+  value = { for k, v in google_compute_instance_group.this : k => v.self_link }
+}
+
+output private_ips {
+  value = { for k, v in google_compute_instance.this :
+    k => [for nic in v.network_interface : nic.network_ip]
+  }
+}
+
 output public_ips {
   value = { for k, v in google_compute_instance.this :
     k => [
       for nic in v.network_interface :
-      try(nic.access_config.0.nat_ip, null)
+      try(nic.access_config[0].nat_ip, null)
   ] }
+}
+
+output nic0_private_ips {
+  value = { for k, v in google_compute_instance.this : k =>
+    try(v.network_interface[0].network_ip, null)
+  }
+}
+
+output nic1_private_ips {
+  value = { for k, v in google_compute_instance.this : k =>
+    try(v.network_interface[1].network_ip, null)
+  }
 }
 
 output nic0_public_ips {
   value = { for k, v in google_compute_instance.this :
-    k => v.network_interface.0.access_config.0.nat_ip
-    if try(v.network_interface.0.access_config.0.nat_ip, null) != null
+    k => v.network_interface[0].access_config[0].nat_ip
+    if can(v.network_interface[0].access_config[0].nat_ip)
   }
 }
 
 output nic1_public_ips {
   value = { for k, v in google_compute_instance.this :
-    k => v.network_interface.1.access_config.0.nat_ip
-    if try(v.network_interface.1.access_config.0.nat_ip, null) != null
+    k => v.network_interface[1].access_config[0].nat_ip
+    if can(v.network_interface[1].access_config[0].nat_ip)
   }
 }
 
-output instance_group_self_links {
-  value = { for k, v in google_compute_instance_group.this : k => v.self_link }
+output nic0_ips {
+  description = "Map of IP addresses of interface at index 0, one entry per each instance. Contains public IP if one exists, otherwise private IP."
+  value = { for k, v in google_compute_instance.this :
+    k => can(v.network_interface[0].access_config[0].nat_ip) ? v.network_interface[0].access_config[0].nat_ip : try(v.network_interface[0].network_ip, null)
+  }
+}
+
+output nic1_ips {
+  description = "Map of IP addresses of interface at index 1, one entry per each instance. Contains public IP if one exists, otherwise private IP."
+  value = { for k, v in google_compute_instance.this :
+    k => can(v.network_interface[1].access_config[0].nat_ip) ? v.network_interface[1].access_config[0].nat_ip : try(v.network_interface[1].network_ip, null)
+  }
 }
