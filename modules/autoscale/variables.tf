00bfda8717e5d6926d5eb981c6a48af13b059add
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2020-12-17 12:55:34 +0100

    doc(autoscale): metrics input

--- modules/autoscale/variables.tf
+++ modules/autoscale/variables.tf
@@ -115,6 +115,14 @@ variable pool {
 }
 
 variable autoscaler_metrics {
+  description = <<-EOF
+  The map with the keys being metrics identifiers (e.g. custom.googleapis.com/VMSeries/panSessionUtilization).
+  Each of the contained objects has attribute `target` which is a numerical threshold for a scale-out or a scale-in.
+  Each zonal group grows until it satisfies all the targets.
+
+  Additional optional attribute `type` defines the metric as either `GAUGE` (the default), `DELTA_PER_SECOND`, or `DELTA_PER_MINUTE`.
+  For full specification, see the `metric` inside the [provider doc](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_autoscaler).
+  EOF
   default = {
     "custom.googleapis.com/VMSeries/panSessionUtilization" = {
       target = 70
