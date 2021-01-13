fffdc96f4e096b946d135e81df669e79f6c910c1
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2021-01-13 13:22:52 +0100

    fix(examples): avoid element() wrap-around
    The element() is unneccessary, we don't want to rotate through, see
    https://www.terraform.io/docs/configuration/functions/element.html

--- examples/regional_workspaces/workspaces.tf
+++ examples/regional_workspaces/workspaces.tf
@@ -1,6 +1,6 @@
 locals {
-  workspace_split_region      = split("+", terraform.workspace)
-  region                      = element(local.workspace_split_region, 1)
-  workspace_split_environment = split("-", terraform.workspace)
-  environment                 = element(local.workspace_split_environment, 1)
+  workspace_split        = split("+", terraform.workspace)
+  region                 = local.workspace_split[1]
+  workspace_prefix_split = split("-", local.workspace_split[0])
+  environment            = element(local.workspace_prefix_split, 1)
 }
