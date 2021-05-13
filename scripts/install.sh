d8bfaa3082ecb7ca942fcb60611856b483ee7e0a
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2021-05-13 09:36:23 +0200

    rm superfluous && ampersand joiners

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,32 +16,38 @@ curl -sL https://github.com/terraform-linters/tflint/releases/download/v0.20.3/t
 curl -sL https://releases.hashicorp.com/terraform/0.12.29/terraform_0.12.29_linux_amd64.zip > terraform-0.12.29.zip    & \
 curl -sL https://releases.hashicorp.com/terraform/0.13.7/terraform_0.13.7_linux_amd64.zip > terraform-0.13.7.zip    & \
 curl -sL https://releases.hashicorp.com/terraform/0.14.9/terraform_0.14.9_linux_amd64.zip > terraform-0.14.9.zip    & \
-wait    && \
-echo Finished successfully all parallel downloads ------------------------------------------------------------------    && \
-chmod +x terraform-docs    && \
-mv terraform-docs /usr/local/bin/    && \
-chmod +x tfsec    && \
-mv tfsec /usr/local/bin/    && \
-unzip tflint.zip    && \
-rm tflint.zip    && \
-mv tflint /usr/local/bin/    && \
-unzip terraform-0.12.29.zip    && \
-rm terraform-0.12.29.zip    && \
-mv terraform /usr/local/bin/terraform    && \
-unzip terraform-0.13.7.zip    && \
-rm terraform-0.13.7.zip    && \
-mv terraform /usr/local/bin/terraform-0.13.7    && \
-unzip terraform-0.14.9.zip    && \
-rm terraform-0.14.9.zip    && \
+wait
+echo Finished successfully all parallel downloads ------------------------------------------------------------------
+
+chmod +x terraform-docs
+mv terraform-docs /usr/local/bin/
+chmod +x tfsec
+mv tfsec /usr/local/bin/
+
+unzip tflint.zip
+rm tflint.zip
+mv tflint /usr/local/bin/
+
+unzip terraform-0.12.29.zip
+rm terraform-0.12.29.zip
+mv terraform /usr/local/bin/terraform
+
+unzip terraform-0.13.7.zip
+rm terraform-0.13.7.zip
+mv terraform /usr/local/bin/terraform-0.13.7
+
+unzip terraform-0.14.9.zip
+rm terraform-0.14.9.zip
 mv terraform /usr/local/bin/terraform-0.14.9
 
-git --version    && \
-terraform-docs --version    && \
-tfsec --version    && \
-tflint --version    && \
-terraform version    && \
-echo "Also, the newest release: $(curl -s https://api.github.com/repos/terraform-docs/terraform-docs/releases/latest | grep -o -E "https://.+?-linux-amd64")"    && \
-echo "Also, the newest release: $(curl -s https://api.github.com/repos/tfsec/tfsec/releases/latest | grep -o -E "https://.+?tfsec-linux-amd64")"    && \
+git --version
+terraform-docs --version
+tfsec --version
+tflint --version
+terraform version
+
+echo "Also, the newest release: $(curl -s https://api.github.com/repos/terraform-docs/terraform-docs/releases/latest | grep -o -E "https://.+?-linux-amd64")"
+echo "Also, the newest release: $(curl -s https://api.github.com/repos/tfsec/tfsec/releases/latest | grep -o -E "https://.+?tfsec-linux-amd64")"
 echo "Also, the newest release: $(curl -s https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip")"
 
 python3 -m pip install -r requirements.txt
