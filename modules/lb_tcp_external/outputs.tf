b35affb88fcb7ca56403e70e89782b501dea2ecb
FrankTheTank <franklin@bitsmasher.net>
2021-07-19 09:26:52 -0400

    terraform fmt

--- modules/lb_tcp_external/outputs.tf
+++ modules/lb_tcp_external/outputs.tf
@@ -1,19 +1,19 @@
-output forwarding_rules {
+output "forwarding_rules" {
   description = "The map of created forwarding rules."
   value       = google_compute_forwarding_rule.rule
 }
 
-output ip_addresses {
+output "ip_addresses" {
   description = "The map of IP addresses of the forwarding rules."
   value       = { for k, v in google_compute_forwarding_rule.rule : k => v.ip_address }
 }
 
-output target_pool {
+output "target_pool" {
   description = "The self-link of the target pool."
   value       = google_compute_target_pool.this.self_link
 }
 
-output created_health_check {
+output "created_health_check" {
   description = "The created health check resource. Null if `create_health_check` option was false."
   value       = var.create_health_check ? google_compute_http_health_check.this[0] : null
 }
