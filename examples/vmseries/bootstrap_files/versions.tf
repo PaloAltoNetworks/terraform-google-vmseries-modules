ece5ef4fa5c608e4ed5adb6cde8894bd275884ef
Lukasz Stadnik <lstadnik@paloaltonetworks.com>
2021-08-05 12:22:21 +0200

    refactor(modules): Bump-up terraform version support

--- examples/vmseries/bootstrap_files/versions.tf
+++ examples/vmseries/bootstrap_files/versions.tf
@@ -3,7 +3,6 @@ terraform {
   required_providers {
     null   = { version = "~> 2.1" }
     random = { version = "~> 2.3" }
-    google = { version = "~> 3.30" }
-    # google = { version = "~> 3.38" }  # because uniform_bucket_level_access
+    google = { version = "~> 3.48" }
   }
 }
