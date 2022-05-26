2a9ed48f149fe25f18e1c487fc20561a7b000cf8
Matthew McLimans <mmclimans@paloaltonetworks.com>
2022-05-26 14:47:24 -0400

    docs(examples/hub_spoke_common): Updates to README with new images, updates to variables.tf & outputs.tf, and created spokes.tf for organizational purposes.

--- examples/hub_spoke_common/main.tf
+++ examples/hub_spoke_common/main.tf
@@ -1,5 +1,5 @@
-# --------------------------------------------------------------------------------------------------------------------------
-# Setup Terraform providers, pull the regions availability zones, and create naming prefix as local variable
+# ----------------------------------------------------------------------------------------------------------------
+# Setup providers, pull availability zones, and create name prefix.
 
 data "google_client_config" "main" {
 }
@@ -31,6 +31,8 @@ locals {
 }
 
 
+# ----------------------------------------------------------------------------------------------------------------
+# Create mgmt, untrust, and trust networks
 
 module "vpc_mgmt" {
   source       = "terraform-google-modules/network/google"
@@ -64,10 +66,8 @@ module "vpc_mgmt" {
   ]
 }
 
-
 module "vpc_untrust" {
-  source = "terraform-google-modules/network/google"
-
+  source       = "terraform-google-modules/network/google"
   version      = "~> 4.0"
   project_id   = var.project_id
   network_name = "${local.prefix}untrust-vpc"
@@ -98,8 +98,7 @@ module "vpc_untrust" {
 }
 
 module "vpc_trust" {
-  source = "terraform-google-modules/network/google"
-
+  source                                 = "terraform-google-modules/network/google"
   version                                = "~> 4.0"
   project_id                             = var.project_id
   network_name                           = "${local.prefix}trust-vpc"
@@ -130,13 +129,18 @@ module "vpc_trust" {
   ]
 }
 
+
+# ----------------------------------------------------------------------------------------------------------------
+# Create VM-Series
+
+# Create IAM service account for accessing bootstrap bucket
 module "iam_service_account" {
   source = "../../modules/iam_service_account/"
 
   service_account_id = "${local.prefix}panw-sa"
 }
 
-# Create bucket for bootstrapping the fresh firewall VM.
+# Create storage bucket to bootstrap VM-Series.
 module "bootstrap" {
   source = "../../modules/bootstrap/"
 
@@ -147,6 +151,7 @@ module "bootstrap" {
   }
 }
 
+# Create 2 VM-Series firewalls
 module "vmseries" {
   for_each = local.vmseries
   source   = "../../modules/vmseries"
@@ -154,19 +159,19 @@ module "vmseries" {
   name                  = "${local.prefix}${each.key}"
   zone                  = each.value.zone
   ssh_keys              = fileexists(var.public_key_path) ? "admin:${file(var.public_key_path)}" : ""
-  vmseries_image        = var.vmseries_common.vmseries_image
+  vmseries_image        = var.fw_image_name
   create_instance_group = true
 
-  bootstrap_options = merge({
+  metadata = {
+    mgmt-interface-swap                  = "enable"
     vmseries-bootstrap-gce-storagebucket = module.bootstrap.bucket_name
-    },
-    var.vmseries_common.bootstrap_options,
-  )
+    serial-port-enable                   = true
+  }
 
   network_interfaces = [
     {
       subnetwork       = module.vpc_untrust.subnets_self_links[0]
-      create_public_ip = true
+      create_public_ip = false
     },
     {
       subnetwork       = module.vpc_mgmt.subnets_self_links[0]
@@ -179,9 +184,9 @@ module "vmseries" {
 }
 
 
+# ----------------------------------------------------------------------------------------------------------------
+# Create internal and external load balancer to distribute traffic to VM-Series
 
-# Due to intranet load balancer solution - DNAT for healthchecks traffic should be configured on firewall.
-# Source: https://knowledgebase.paloaltonetworks.com/KCSArticleDetail?id=kA10g000000PP9QCAW
 module "lb_tcp_internal" {
   source = "../../modules/lb_tcp_internal"
 
@@ -205,209 +210,4 @@ module "lb_tcp_external" {
 
   health_check_http_port         = 80
   health_check_http_request_path = "/"
-}
-
-module "vpc_spoke1" {
-  source                                 = "terraform-google-modules/network/google"
-  version                                = "~> 4.0"
-  project_id                             = var.project_id
-  network_name                           = "${local.prefix}spoke1-vpc"
-  routing_mode                           = "GLOBAL"
-  delete_default_internet_gateway_routes = true
-
-  subnets = [
-    {
-      subnet_name   = "${local.prefix}${var.region}-spoke1"
-      subnet_ip     = var.cidr_spoke1
-      subnet_region = var.region
-    }
-  ]
-
-  routes = [
-    {
-      name              = "${local.prefix}spoke1-to-ilbnh"
-      description       = "Default route to VM-Series NGFW"
-      destination_range = "0.0.0.0/0"
-      next_hop_ilb      = cidrhost(var.cidr_trust, 10)
-      #tags             = "egress-inet"
-    }
-  ]
-
-  firewall_rules = [
-    {
-      name      = "${local.prefix}allow-all-spoke1"
-      direction = "INGRESS"
-      priority  = "100"
-      ranges    = ["0.0.0.0/0"]
-      allow = [
-        {
-          protocol = "all"
-          ports    = []
-        }
-      ]
-    }
-  ]
-}
-
-module "vpc_spoke2" {
-  source                                 = "terraform-google-modules/network/google"
-  version                                = "~> 4.0"
-  project_id                             = var.project_id
-  network_name                           = "${local.prefix}spoke2-vpc"
-  routing_mode                           = "GLOBAL"
-  delete_default_internet_gateway_routes = true
-
-  subnets = [
-    {
-      subnet_name   = "${local.prefix}${var.region}-spoke2"
-      subnet_ip     = var.cidr_spoke2
-      subnet_region = var.region
-    }
-  ]
-
-  routes = [
-    {
-      name              = "${local.prefix}spoke2-to-ilbnh"
-      description       = "Default route to VM-Series NGFW"
-      destination_range = "0.0.0.0/0"
-      next_hop_ilb      = cidrhost(var.cidr_trust, 10)
-      #next_hop_internet = "true"
-      #tags             = "egress-inet"
-    }
-  ]
-
-  firewall_rules = [
-    {
-      name      = "${local.prefix}allow-all-spoke2"
-      direction = "INGRESS"
-      priority  = "100"
-      ranges    = ["0.0.0.0/0"]
-      allow = [
-        {
-          protocol = "all"
-          ports    = []
-        }
-      ]
-    }
-  ]
-}
-
-
-# # --------------------------------------------------------------------------------------------------------------------------
-# # Create VPC peering connections between spoke networks and the trust network
-
-resource "google_compute_network_peering" "spoke1_to_trust" {
-  name         = "${local.prefix}spoke1-to-trust"
-  network      = module.vpc_spoke1.network_id
-  peer_network = module.vpc_trust.network_id
-}
-
-resource "google_compute_network_peering" "trust_to_spoke1" {
-  name         = "${local.prefix}trust-to-spoke1"
-  network      = module.vpc_trust.network_id
-  peer_network = module.vpc_spoke1.network_id
-}
-
-
-resource "google_compute_network_peering" "spoke2_to_trust" {
-  name         = "${local.prefix}spoke2-to-trust"
-  network      = module.vpc_spoke2.network_id
-  peer_network = module.vpc_trust.network_id
-}
-
-resource "google_compute_network_peering" "trust_to_spoke2" {
-  name         = "${local.prefix}trust-to-spoke2"
-  network      = module.vpc_trust.network_id
-  peer_network = module.vpc_spoke2.network_id
-}
-
-
-# --------------------------------------------------------------------------------------------------------------------------
-# Create spoke1 compute instances with internal load balancer
-
-resource "google_compute_instance" "spoke1_vm" {
-  count                     = 2
-  name                      = "${local.prefix}spoke1-vm${count.index + 1}"
-  machine_type              = var.spoke_vm_type
-  zone                      = data.google_compute_zones.main.names[0]
-  can_ip_forward            = false
-  allow_stopping_for_update = true
-
-  metadata = {
-    serial-port-enable = true
-    ssh-keys           = fileexists(var.public_key_path) ? "${var.spoke_vm_user}:${file(var.public_key_path)}" : ""
-  }
-
-  network_interface {
-    subnetwork = module.vpc_spoke1.subnets_self_links[0]
-    #network_ip = cidrhost(var.cidr_spoke1, 10)
-  }
-
-  boot_disk {
-    initialize_params {
-      image = var.spoke_vm_image
-    }
-  }
-
-  service_account {
-    scopes = var.spoke_vm_scopes
-  }
-}
-
-
-resource "google_compute_instance_group" "spoke1_ig" {
-  name = "${local.prefix}spoke1-ig"
-  zone = data.google_compute_zones.main.names[0]
-
-  instances = google_compute_instance.spoke1_vm.*.id
-}
-
-module "spoke1_ilb" {
-  source = "../../modules/lb_tcp_internal"
-
-  name     = "${local.prefix}spoke1-ilb"
-  backends = { 0 = google_compute_instance_group.spoke1_lb.self_link }
-  # backends = tomap({"0" = google_compute_instance_group.spoke1_lb.self_link})
-  ip_address = cidrhost(var.cidr_spoke1, 10)
-  subnetwork = module.vpc_spoke1.subnets_self_links[0]
-  network    = module.vpc_spoke1.network_id
-
-  all_ports = false
-
-  timeout_sec       = 1
-  ports             = [80]
-  health_check_port = 80
-
-}
-
-
-# --------------------------------------------------------------------------------------------------------------------------
-# Create spoke2 compute instances. 
-
-resource "google_compute_instance" "spoke2_vm1" {
-  name                      = "${local.prefix}spoke2-vm1"
-  machine_type              = var.spoke_vm_type
-  zone                      = data.google_compute_zones.main.names[0]
-  can_ip_forward            = false
-  allow_stopping_for_update = true
-
-  metadata = {
-    serial-port-enable = true
-    ssh-keys           = fileexists(var.public_key_path) ? "${var.spoke_vm_user}:${file(var.public_key_path)}" : ""
-  }
-
-  network_interface {
-    subnetwork = module.vpc_spoke2.subnets_self_links[0]
-    network_ip = cidrhost(var.cidr_spoke2, 10)
-  }
-
-  boot_disk {
-    initialize_params {
-      image = var.spoke_vm_image
-    }
-  }
-
-  service_account {
-    scopes = var.spoke_vm_scopes
-  }
 }
