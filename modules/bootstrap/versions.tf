d62647abf9c0f80653562d45343fc8b5e80eb4e2
James <jamoiholland@gmail.com>
2022-06-04 15:50:15 +0100

    Upgrade provider versions for Mac M1 machines

--- modules/bootstrap/versions.tf
+++ modules/bootstrap/versions.tf
@@ -1,8 +1,8 @@
 terraform {
   required_version = ">= 0.15.3, < 2.0"
   required_providers {
-    null   = { version = "~> 2.1" }
-    random = { version = "~> 2.3" }
+    null   = { version = "~> 3.1" }
+    random = { version = "~> 3.1" }
     google = { version = "~> 3.30" }
     # google = { version = "~> 3.38" }  # because uniform_bucket_level_access
   }
