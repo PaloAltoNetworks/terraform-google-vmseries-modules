ff91adba3178dc6951cbdc778fed336e22fa46b1
Matthew McLimans <mmclimans@paloaltonetworks.com>
2022-06-14 12:36:56 -0400

    fix(examples/autoscale): update versions.tf

--- examples/autoscale/versions.tf
+++ examples/autoscale/versions.tf
@@ -2,8 +2,6 @@ terraform {
   required_version = ">= 0.15.3, < 2.0"
 
   required_providers {
-    null   = { version = "~> 3.1" }
-    random = { version = "~> 3.1" }
     google = { version = "~> 3.48" }
   }
 }
