5cf11bae7e593eb045160d81b2c883f30ec6c3dc
Lukasz Stadnik <lstadnik@paloaltonetworks.com>
2022-08-11 12:50:52 +0200

    fix(module/autoscale): remove provider version

--- modules/autoscale/main.tf
+++ modules/autoscale/main.tf
@@ -1,11 +1,5 @@
 terraform {
   required_providers {
-    null = {
-      version = "~> 2.1"
-    }
-    random = {
-      version = "~> 2.3"
-    }
     google = {
       version = "~> 3.48"
     }
