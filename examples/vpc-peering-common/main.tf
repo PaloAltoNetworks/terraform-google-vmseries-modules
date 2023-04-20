aff60de65c8b5e5df93bd19bbab4a46fd9d72247
Horia Gunica <Horia.Gunica@gmail.com>
2023-04-20 13:52:38 +0300

    modified structure for bootstrap_options

--- examples/vpc-peering-common/main.tf
+++ examples/vpc-peering-common/main.tf
@@ -115,9 +115,13 @@ module "vmseries" {
   scopes                = try(each.value.scopes, [])
   create_instance_group = true
 
-  bootstrap_options = try(merge({ vmseries-bootstrap-gce-storagebucket = "${module.bootstrap[each.value.bootstrap-bucket-key].bucket_name}/${each.key}/" }, var.vmseries_common.bootstrap_options), merge(
-    try(each.value.bootstrap_options, {}),
-    try(var.vmseries_common.bootstrap_options, {})
+  bootstrap_options = try(
+    merge(
+    { vmseries-bootstrap-gce-storagebucket = "${module.bootstrap[each.value.bootstrap-bucket-key].bucket_name}/${each.key}/" },
+    var.vmseries_common.bootstrap_options),
+    merge(
+     try(each.value.bootstrap_options, {}),
+     try(var.vmseries_common.bootstrap_options, {})
   ))
 
   named_ports = try([for v in each.value.named_ports :
