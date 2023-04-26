f05c354eb5c043d901eeb2d5e1a24da6a3974888
Horia Gunica <43091730+horiagunica@users.noreply.github.com>
2023-04-26 11:31:56 +0300

    Update examples/panorama/outputs.tf
    Co-authored-by: michalbil <92343355+michalbil@users.noreply.github.com>

--- examples/panorama/outputs.tf
+++ examples/panorama/outputs.tf
@@ -3,7 +3,7 @@ output "panorama_private_ips" {
   value       = { for k, v in module.panorama : k => v.panorama_private_ip }
 }
 
-output "panorama_public_ip" {
+output "panorama_public_ips" {
   description = "Public IP address of the Panorama instance."
   value       = { for k, v in module.panorama : k => v.panorama_public_ip }
 }
