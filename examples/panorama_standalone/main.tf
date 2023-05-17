6fe3f0fbd9455bb2a762d571ded8502bcbb50ebe
Horia Gunica <43091730+horiagunica@users.noreply.github.com>
2023-05-17 18:43:29 +0300

    example(panorama_standalone): panorama example refactor (#173)

---
+++ examples/panorama_standalone/main.tf
@@ -0,0 +1,27 @@
+module "vpc" {
+  source = "../../modules/vpc"
+
+  networks = { for k, v in var.networks : k => merge(v, {
+    name            = "${var.name_prefix}${v.name}"
+    subnetwork_name = "${var.name_prefix}${v.subnetwork_name}"
+    })
+  }
+}
+
+module "panorama" {
+  source = "../../modules/panorama"
+
+  for_each = var.panoramas
+
+  name              = "${var.name_prefix}${each.value.panorama_name}"
+  project           = var.project
+  region            = var.region
+  zone              = each.value.zone
+  panorama_version  = each.value.panorama_version
+  ssh_keys          = each.value.ssh_keys
+  subnet            = module.vpc.subnetworks["${var.name_prefix}${each.value.panorama_subnet}"].self_link
+  private_static_ip = each.value.private_static_ip
+  attach_public_ip  = each.value.attach_public_ip
+  log_disks         = each.value.log_disks
+  depends_on        = [module.vpc]
+}
