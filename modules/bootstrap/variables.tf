ba5619666477691bc220816a1b30bab6820a91c2
Horia Gunica <Horia.Gunica@gmail.com>
2023-07-13 16:42:33 +0300

    Fixed bootstrap bucket backwards compat

--- modules/bootstrap/variables.tf
+++ modules/bootstrap/variables.tf
@@ -23,9 +23,12 @@ variable "location" {
 
 
 variable "bootstrap_files_dir" {
-  description = "Bootstrap file directory. More information can be found at https://docs.paloaltonetworks.com/vm-series/9-1/vm-series-deployment/bootstrap-the-vm-series-firewall/bootstrap-package"
+  description = <<-EOF
+  Bootstrap file directory. If the variable has a value of `null` (default) - then it will not upload any other files other than the ones specified in the `files` variable.
+  More information can be found at https://docs.paloaltonetworks.com/vm-series/9-1/vm-series-deployment/bootstrap-the-vm-series-firewall/bootstrap-package.
+  EOF
   type        = string
-  default     = ""
+  default     = null
 }
 
 
