836c65978a88e2cbe53d5207d04e199b00ae3358
michalbil <92343355+michalbil@users.noreply.github.com>
2023-07-13 09:57:52 +0200

    feat(modules/lb_external)!: Enhance connection tracking support (#163)

--- modules/lb_external/variables.tf
+++ modules/lb_external/variables.tf
@@ -27,10 +27,42 @@ variable "rules" {
   EOF
 }
 
+variable "instances" {
+  description = "List of links to the instances. Expected to be empty when using an autoscaler, as the autoscaler inserts entries to the target pool dynamically. The nic0 of each instance gets the traffic. Even when this list is shifted or re-ordered, it doesn't re-create any resources and such modifications often proceed without any noticeable downtime."
+  type        = list(string)
+  default     = null
+}
+
+variable "backend_instance_groups" {
+  description = "List of backend instance groups"
+  default     = []
+}
+
 variable "session_affinity" {
-  description = "How to distribute load. Options are `NONE`, `CLIENT_IP` and `CLIENT_IP_PROTO`."
+  description = <<-EOF
+  Controls distribution of new connections (or fragmented UDP packets) from clients to the backends, can influence available connection tracking configurations.
+  Valid values are: NONE (default), CLIENT_IP, CLIENT_IP_PROTO, CLIENT_IP_PORT_PROTO (only available for backend service based rules).
+  EOF
   type        = string
   default     = "NONE"
+}
+
+variable "connection_tracking_policy" {
+  description = <<-EOF
+  Connection tracking policy settings, only available for backend service based rules. Following options are available:
+  - `mode`                              - (Optional|string) `PER_CONNECTION` (default) or `PER_SESSION`
+  - `persistence_on_unhealthy_backends` - (Optional|string) `DEFAULT_FOR_PROTOCOL` (default), `ALWAYS_PERSIST` or `NEVER_PERSIST`
+
+  More information about supported configurations in conjunction with `session_affinity` is available in [Backend service-based external Network Load Balancing](https://cloud.google.com/load-balancing/docs/network/networklb-backend-service#connection-tracking) documentation.
+  EOF
+  default     = null
+  type        = map(any)
+}
+
+variable "network_tier" {
+  description = "The networking tier used for configuring this address. If this field is not specified, it is assumed to be PREMIUM. Possible values are PREMIUM and STANDARD."
+  type        = string
+  default     = "PREMIUM"
 }
 
 variable "create_health_check" {
@@ -79,39 +111,4 @@ variable "health_check_http_host" {
   description = "Health check http request host header, with the default adjusted to localhost to be able to check the health of the PAN-OS webui."
   type        = string
   default     = "localhost"
-}
-
-variable "instances" {
-  description = "List of links to the instances. Expected to be empty when using an autoscaler, as the autoscaler inserts entries to the target pool dynamically. The nic0 of each instance gets the traffic. Even when this list is shifted or re-ordered, it doesn't re-create any resources and such modifications often proceed without any noticeable downtime."
-  type        = list(string)
-  default     = null
-}
-
-variable "connection_tracking_mode" {
-  description = "There are two options: PER_CONNECTION: The Connection Tracking is performed as per the Connection Key (default Hash Method) for the specific protocol. PER_SESSION: The Connection Tracking is performed as per the configured Session Affinity. It matches the configured Session Affinity."
-  type        = string
-  default     = "PER_CONNECTION"
-}
-
-variable "connection_persistence_on_unhealthy_backends" {
-  description = "Specifies connection persistence when backends are unhealthy. If set to DEFAULT_FOR_PROTOCOL, the existing connections persist on unhealthy backends only for connection-oriented protocols (TCP and SCTP) and only if the Tracking Mode is PER_CONNECTION (default tracking mode) or the Session Affinity is configured for 5-tuple. They do not persist for UDP. If set to NEVER_PERSIST, after a backend becomes unhealthy, the existing connections on the unhealthy backend are never persisted on the unhealthy backend. They are always diverted to newly selected healthy backends (unless all backends are unhealthy). If set to ALWAYS_PERSIST, existing connections always persist on unhealthy backends regardless of protocol and session affinity. It is generally not recommended to use this mode overriding the default."
-  type        = string
-  default     = "DEFAULT_FOR_PROTOCOL"
-}
-
-variable "idle_timeout_sec" {
-  description = "Specifies how long to keep a Connection Tracking entry while there is no matching traffic (in seconds). For L4 ILB the minimum(default) is 10 minutes and maximum is 16 hours. For NLB the minimum(default) is 60 seconds and the maximum is 16 hours."
-  type        = number
-  default     = null
-}
-
-variable "network_tier" {
-  description = "The networking tier used for configuring this address. If this field is not specified, it is assumed to be PREMIUM. Possible values are PREMIUM and STANDARD."
-  type        = string
-  default     = "PREMIUM"
-}
-
-variable "backend_instance_groups" {
-  description = "List of backend instance groups"
-  default     = []
 }
