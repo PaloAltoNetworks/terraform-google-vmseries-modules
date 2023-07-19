0af0d63ff63739c4d8c61c435196e382c8d5fc49
pavelrn <30939390+pavelrn@users.noreply.github.com>
2023-07-19 18:15:03 +0200

    Deleting delicense as separate example

--- examples/autoscale_delicense/versions.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_version = ">= 1.2, < 2.0"
-}
-
-provider "google" {
-  project = var.project
-  region  = var.region
-}
--- modules/autoscale_delicense/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "cfn_details" {
-  value = google_cloudfunctions_function.this
-}
