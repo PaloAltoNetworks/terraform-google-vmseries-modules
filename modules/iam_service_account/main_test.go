56cdd61be2598b179a4909eb7ccacf4d4efce89d
Horia Gunica <Horia.Gunica@gmail.com>
2023-09-21 14:17:23 +0300

    Removed makefiles and added go for modules

---
+++ modules/iam_service_account/main_test.go
@@ -0,0 +1,11 @@
+package iam_service_account
+
+import (
+	"testing"
+
+	"github.com/PaloAltoNetworks/terraform-modules-vmseries-tests-skeleton/pkg/testskeleton"
+)
+
+func TestValidate(t *testing.T) {
+	testskeleton.ValidateCode(t, nil)
+}
