49d80570474b9b017b91d65f4311f9e79035d08a
Horia Gunica <Horia.Gunica@gmail.com>
2023-11-09 11:11:57 +0100

    Fixed outputs

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,12 +1,5 @@
 output "subnetworks" {
-  value = { for _, v in var.subnetworks : v.subnetwork_name
-    => try(data.google_compute_subnetwork.this[v.subnetwork_name], google_compute_subnetwork.this[v.subnetwork_name], null)
-  }
-}
-
-output "subnetworks_by_key" {
-  description = "Map with subnetwork objects corresponding to input key (or index if list was provided) of `networks` variable."
   value = { for k, v in var.subnetworks :
-    k => try(data.google_compute_subnetwork.this[v.subnetwork_name], google_compute_subnetwork.this[v.subnetwork_name])
+    k => try(data.google_compute_subnetwork.this[v.subnetwork_name], google_compute_subnetwork.this[v.subnetwork_name], null)
   }
 }
