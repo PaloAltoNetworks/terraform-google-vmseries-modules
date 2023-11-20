7d78d6eb2dacbdeed3078911491fe3035c30f2ee
Horia Gunica <Horia.Gunica@gmail.com>
2023-11-20 13:20:55 +0200

    Reordered outputs

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,8 +1,8 @@
+output "network" {
+  value = try(data.google_compute_network.this[0], google_compute_network.this[0])
+}
 output "subnetworks" {
   value = { for k, v in var.subnetworks :
     k => try(data.google_compute_subnetwork.this[v.subnetwork_name], google_compute_subnetwork.this[v.subnetwork_name], null)
   }
-}
-output "network" {
-  value = try(data.google_compute_network.this[0], google_compute_network.this[0])
 }
