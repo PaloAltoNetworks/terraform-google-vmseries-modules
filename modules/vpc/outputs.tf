36218efab4f6787f198c1367b1e4c212b2152dd3
Horia Gunica <43091730+horiagunica@users.noreply.github.com>
2023-11-21 18:08:57 +0200

    Update modules/vpc/outputs.tf
    Co-authored-by: michalbil <92343355+michalbil@users.noreply.github.com>

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -2,6 +2,7 @@ output "network" {
   description = "Created or read network attributes."
   value       = try(data.google_compute_network.this[0], google_compute_network.this[0])
 }
+
 output "subnetworks" {
   description = "Map containing key, value pairs of created or read subnetwork attributes."
   value = { for k, v in var.subnetworks :
