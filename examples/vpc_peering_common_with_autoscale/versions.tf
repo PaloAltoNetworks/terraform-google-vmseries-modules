55cbf64f7f3c3921682f3ea6710cd6edd321bb28
Horia Gunica <Horia.Gunica@gmail.com>
2023-11-27 12:47:10 +0200

    Removed old autoscale example - WIP autoscale refactor

--- examples/vpc_peering_common_with_autoscale/versions.tf
+++ examples/vpc_peering_common_with_autoscale/versions.tf
@@ -1,16 +1,13 @@
 terraform {
   required_version = ">= 1.3, < 2.0"
-  required_providers {
-    google = { version = "~> 4.58" }
-  }
 }
 
 provider "google" {
-  project = var.project_id
+  project = var.project
   region  = var.region
 }
 
 provider "google-beta" {
-  project = var.project_id
+  project = var.project
   region  = var.region
 }
